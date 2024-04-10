917609b70d8f4912fdb998d9ceb30760f5d06362
andrewhdz130794 <160076399+andrewhdz130794@users.noreply.github.com>
2024-04-09 23:02:50 -0600

    Update DB_transaccional.sql

--- DB_transaccional.sql
+++ DB_transaccional.sql
@@ -1,5 +1,10 @@
+create table categoria(
+idcategoria int auto_increment primary key,
+nombre varchar(50),
+descripcion varchar(255),
+estado bit
+);
 
-/* Entities */
 CREATE TABLE articulo (
     idarticulo INT AUTO_INCREMENT PRIMARY KEY,
     idcategoria INT NOT NULL,
@@ -13,6 +18,17 @@ CREATE TABLE articulo (
     FOREIGN KEY (idcategoria) REFERENCES categoria(idcategoria)
 );
 
+create table ingreso(
+idingreso int auto_increment primary key,
+idproveedor int not null,
+idusuario int not null,
+tipo_comprobante varchar(20),
+serie_comprobante varchar(7),
+num_comprobante varchar(10),
+fecha datetime,
+impuesto decimal(4,2),
+total decimal(11,2),
+estado varchar(20));
 
 CREATE TABLE persona (
     idpersona INT AUTO_INCREMENT PRIMARY KEY,
@@ -23,11 +39,13 @@ CREATE TABLE persona (
     direccion VARCHAR(70),
     telefono VARCHAR(20),
     email VARCHAR(50),
-    FOREIGN KEY (idpersona) REFERENCES venta(idcliente),
     FOREIGN KEY (idpersona) REFERENCES ingreso(idproveedor)
 );
-
-
+CREATE TABLE rol (
+    idrol INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(30),
+    descripcion VARCHAR(255),
+    estado BIT);
 CREATE TABLE usuario (
     idusuario INT AUTO_INCREMENT PRIMARY KEY,
     idrol INT,
@@ -39,27 +57,11 @@ CREATE TABLE usuario (
     email VARCHAR(50),
     clave VARBINARY(255),
     estado BIT,
-    FOREIGN KEY (idrol) REFERENCES rol(idrol),
     FOREIGN KEY (idusuario) REFERENCES ingreso(idusuario),
-    FOREIGN KEY (idusuarioventa) REFERENCES venta(idusuario)
-);
-/* Tablas uno a uno con las Entities */
-create table categoria(
-idcategoria int auto_increment primary key,
-nombre varchar(50),
-descripcion varchar(255),
-estado bit
+    FOREIGN KEY (idusuarioventa) REFERENCES venta(idusuario),
+    FOREIGN KEY (idusuario) REFERENCES ingreso(idusuario)
 );
 
-CREATE TABLE rol (
-    idrol INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(30),
-    descripcion VARCHAR(255),
-    estado BIT,
-    FOREIGN KEY (idrol) REFERENCES usuario(idrol)
-);
-
-/* Tablas venta e ingreso */
 create table venta(
 idventa int auto_increment primary key,
 idcliente int not null,
@@ -71,31 +73,8 @@ fecha datetime,
 impuesto decimal(4,2),
 total decimal(11,2),
 estado varchar(20),
-constraint fk_idcliente foreign key (idcliente) references persona(idpersona)
-	on delete set null
-	on update cascade,
-constraint fk_idusuario_vta foreign key (idusuario) references usuario(idusuario)
-	on delete set null
-	on update cascade
-);
-
-create table ingreso(
-idingreso int auto_increment primary key,
-idproveedor int not null,
-idusuario int not null,
-tipo_comprobante varchar(20),
-serie_comprobante varchar(7),
-num_comprobante varchar(10),
-fecha datetime,
-impuesto decimal(4,2),
-total decimal(11,2),
-estado varchar(20),
-constraint fk_idproveedor foreign key (idproveedor) references persona(idpersona)
-	on delete set null
-	on update cascade,
-constraint fk_idusuario_ing foreign key (idusuario) references usuario(idusuario)
-	on delete set null
-	on update cascade
+FOREIGN KEY (idcliente) REFERENCES persona(idpersona)
+FOREIGN KEY (idusuario) REFERENCES persona(idusuario)
 );
 
 /* Tablas detalle */
@@ -105,12 +84,8 @@ idingreso int not null,
 idarticulo int not null,
 cantidad int not null,
 precio decimal(11,2),
-constraint fk_idingreso foreign key (idingreso) references ingreso(idingreso)
-	on delete cascade
-	on update cascade,
-constraint fk_idarticulo_ing foreign key (idarticulo) references articulo(idarticulo)
-	on delete cascade
-	on update cascade
+foreign key (idingreso) references ingreso(idingreso)
+foreign key (idarticulo) references articulo(idarticulo)
 );
 
 create table detalle_venta(
@@ -120,12 +95,8 @@ idarticulo int not null,
 cantidad int not null,
 precio decimal(11,2),
 descuento decimal(11,2),
-constraint fk_idventa foreign key (idventa) references venta(idventa)
-	on delete cascade
-	on update cascade,
-constraint fk_idarticulo_vta foreign key (idarticulo) references articulo(idarticulo)
-	on delete cascade
-	on update cascade
+foreign key (idventa) references venta(idventa)
+foreign key (idarticulo) references articulo(idarticulo)
 );
 
 
